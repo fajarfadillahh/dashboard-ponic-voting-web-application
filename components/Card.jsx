228a61e8cae6d89909977ee6732151fea7170b89
fajarfadillahh <fajaragustian979@gmail.com>
2023-10-03 23:00:49 +0700

    create Card components for dashboard page

---
+++ components/Card.jsx
@@ -0,0 +1,27 @@
+import { Typography } from "@material-tailwind/react";
+
+export default function Card({ widget }) {
+  return (
+    <div className="flex h-max min-w-[320px] flex-1 justify-center rounded-md border border-gray-100 p-8">
+      <div className="w-full max-w-[265px]">
+        <Typography className="text-xl font-semibold capitalize text-gray-900">
+          {widget.title}
+          <span className="text-pink-500">:</span>
+        </Typography>
+
+        <div className="text-center">
+          <Typography className="-mb-5 text-[82px] font-extrabold text-pink-500">
+            {widget.amount}
+          </Typography>
+          <Typography className="font-semibold text-gray-500">
+            {widget.title === "total users"
+              ? "User"
+              : widget.title === "total rooms"
+              ? "Rooms"
+              : "Candidates"}
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,22 @@ import Head from "next/head";
 // import components
 import Layout from "@/components/Layout";
 import Title from "@/components/Title";
+import Card from "@/components/Card";
+
+const data = [
+  {
+    title: "total users",
+    amount: 48,
+  },
+  {
+    title: "total rooms",
+    amount: 27,
+  },
+  {
+    title: "total candidates",
+    amount: 79,
+  },
+];
 
 export default function Home() {
   return (
@@ -12,8 +28,14 @@ export default function Home() {
       </Head>
 
       <Layout>
-        <section>
+        <section className="grid gap-6">
           <Title text="Dashboard" />
+
+          <div className="flex flex-wrap items-center gap-6">
+            {data.map((widget, index) => {
+              return <Card key={index} widget={widget} />;
+            })}
+          </div>
         </section>
       </Layout>
     </>
